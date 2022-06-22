7681472fddccda7466b89bb8f2c583d466cafa32
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2022-06-22 19:44:11 +0200

    Update CurdApplication.sql

--- CurdApplication.sql
+++ CurdApplication.sql
@@ -10,19 +10,3 @@ create table accounts (
 	phoneNumber int not null,
 	gender varchar(6) NOT NULL CHECK (gender IN('Male', 'Female'))
 );
-update accounts SET
-	fristName = @fName,
-	lastName = @lName,
-	email = @email,
-	phoneNumber = @phone,
-	gender = @gender
-		WHERE ID = @ID;
-
-select * from accounts;
--- insert quiry
-
-Select * from accounts where fristName like '%@fName%';
-
-insert into accounts (firstName, lastName,email,phoneNumber,gender)
-                                            values
-                                            (@fName, @lName, @email, @phone, @gender)
